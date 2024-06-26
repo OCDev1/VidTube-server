abb64872e6c848a45165551ca2c87a988e464de5
Omri Cohen <omricohen19@gmail.com>
2024-06-26 23:12:26 +0300

    need to fix routes for comment by id,edit,delete

--- routes/comment.js
+++ routes/comment.js
@@ -5,14 +5,17 @@ var router = express.Router();
 
 router.route('/comments')
     .get(commentController.getComments)
+
     .post(commentController.createComment);
 
-router.route('/comments/:id')
+router.route('/users/:id')
+
+router.route('/users/:id/comments')
     .get(commentController.getComment)
     .patch(commentController.updateComment)
     .delete(commentController.deleteComment)
 
-router.route('/comments/:videoId')
+router.route('/users/id/comments/:pid')
     .get(commentController.getCommentsByVideoId);
 
 module.exports = router;
